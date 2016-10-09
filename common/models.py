86503b5fe3ba77a1f32081851b1e372005c6d2cf
Chao Lei <leichao139636@163.com>
2016-10-09 12:02:06 +0800

    均值方法二

--- common/models.py
+++ common/models.py
@@ -11,10 +11,15 @@ class RULES(object):
         for i in range(len(self.pre_time)):
             self.pre_time[i] = self.pre_time[i][0:-1]
         self.pre_WIFIAPTag = pre_WIFIAPTag
+        print (self.pre_time)
         print("RULES is inited OK!")
 
-    def mean_solution(self):
-        print("Mean_solution Run")
+    def mean_solution_one(self):
+        '''
+        均值规则一：
+        取总体的平均，作为结果
+        '''
+        print("Mean_solution_one Run!")
         pre_value = self.WIFI_Records.groupby("WIFIAPTag")["passengerCount"].mean().round(1).reset_index()
         # 转换成标准格式
         lst = list(1 for i in range(1, len(self.pre_time)+1))
@@ -22,9 +27,45 @@ class RULES(object):
         pre_value['flag'] = 1
         pre_value = pre_value.merge(trans, how='left', on='flag')
         del pre_value['flag']
-        pd.DataFrame(pre_value, columns=['PassengerCount', 'WIFIAPTag', 'timeStamp'])
+        pd.DataFrame(pre_value, columns=['PassengerCount', 'WIFIAPTag', 'slice10min'])
         # pre_value.to_csv("submissions/airport_gz_passenger_predict_mean.csv", index=True)
+        print("Mean_solution_one END!")
         return pre_value
+    def mean_solution_two(self):
+        print("Mean_solution_two Run!")
+        train_data = self.WIFI_Records
+        train_data["slice10min"] = train_data["timeStamp"].apply(lambda x: x[0:15])
+        # train_data = train_data.sort_values(by=["WIFIAPTag", "slice10min"])
+        train_data = train_data.groupby(by=["WIFIAPTag", "slice10min"])["passengerCount"].sum().reset_index()
+        train_data["passengerCount"] = (train_data["passengerCount"]/10).round(2)
+        train_data["time"] = train_data["slice10min"].apply(lambda x: x[-4:])
+        train_data = train_data.groupby(by=["time", "WIFIAPTag"])["passengerCount"].mean().round(2).reset_index()
+        pre_oldtime = [every_time[-4:] for every_time in self.pre_time]
+        train_data["slice10min"] = train_data["time"].apply(lambda x: "2016-09-14-"+x)
+        del train_data["time"]
+        train_data["slice10min"] == "2016-09-14-15-0"
+        pre_value = train_data.loc[train_data["slice10min"].isin(self.pre_time),
+                                   ["passengerCount", "WIFIAPTag", "slice10min"]]
+        pre_value.to_csv("submissions/train_data.csv", index=False)
+        '''
+        print (pre_oldtime)
+        # for group in train_data.groupby(by=["WIFIAPTag"]):
+            # print group[1]["slice10min"]
+            # break
+            # group[1]["slice10min"]
+        for group in train_data.groupby(by=["time"]):
+            # print group[0]
+            # if group[0] in pre_oldtime:
+
+
+            break
+        '''
+
+        # train_data.to_csv("clean_data/train_data.csv", index=False)
+        print("Mean_solution_two END!")
+        return pre_value
+
+
 
 
 
--- rule_solution.py
+++ rule_solution.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
 from common.models import *
 
 def plot_wifi_records(WIFI_Records):
@@ -25,7 +26,7 @@ def plot_wifi_records(WIFI_Records):
     print("plot_wifi_records Done")
 
 
-def rule_solution():
+def rule_solution(filename, start_time, end_time):
     print("Run in rule_solution!")
     '''
     WIFI_Records = pd.read_csv("../input/WIFI_AP_Passenger_Records_chusai_1stround.csv",
@@ -33,18 +34,44 @@ def rule_solution():
                                date_parser=(lambda dt: pd.Timestamp(dt))
                                )
     '''
-    WIFI_Records = pd.read_csv("../input/WIFI_AP_Passenger_Records_chusai_1stround.csv")
+    WIFI_Records = pd.read_csv(filename)
     # WIFI_Records["timeStamp"] = WIFI_Records["timeStamp"].apply(lambda x: pd.Timestamp(x))
-    pre_time = pd.date_range(start="20160914150000", end="20160914175959", freq="10min")
+    pre_time = pd.date_range(start=start_time, end=end_time, freq="10min")
     pre_WIFIAPTag = WIFI_Records["WIFIAPTag"].drop_duplicates()
-    plot_wifi_records(WIFI_Records)  # 每个wifi接入点中，时间与人数的关系
-
-    # 方法一：取均值
+    #  plot_wifi_records(WIFI_Records)  # 每个wifi接入点中，时间与人数的关系
     rule = RULES(WIFI_Records, pre_time, pre_WIFIAPTag)
-    rule_res_mean = rule.mean_solution()
+
+    # 方法一：取均值一
+    # rule_res_mean = rule.mean_solution_one()
+    # 方法二：取均值二
+    rule_res_mean = rule.mean_solution_two()
+
     return rule_res_mean
 
-if __name__ == "__main__":
+def test(result):
+    test_result = pd.read_csv("clean_data/airport_gz_passenger_predict_standard.csv")
+    Criteria = sum((result['passengerCount']-test_result['passengerCount'])*(result['passengerCount']-test_result['passengerCount']))
+    print("Criteria = %f" % Criteria)
 
-    rule_result = rule_solution()
-    rule_result.to_csv("submissions/airport_gz_passenger_predict_mean.csv", index=False)
+if __name__ == "__main__":
+    time0 = time.time()
+    '''
+    # 测试集
+    filename = "clean_data/test_train.csv"
+    start_time = "20160914120000"
+    end_time = "20160914145959"
+    '''
+
+
+    # 真正训练集
+    filename = "../input/WIFI_AP_Passenger_Records_chusai_1stround.csv"
+    start_time = "20160914150000"
+    end_time = "20160914175959"
+
+
+    rule_result = rule_solution(filename, start_time, end_time)
+    rule_result.to_csv("submissions/airport_gz_passenger_predict_mean_two.csv", index=False)
+
+    # test(rule_result)
+    time1 = time.time()
+    print ("It take %f s to process" % (time1 - time0))
