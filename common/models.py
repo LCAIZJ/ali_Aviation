15fbe7c1e4e71dea9aab87cf8ed939eb2c6ccf9d
Chao Lei <leichao139636@163.com>
2016-10-13 21:26:02 +0800

    模型方法：使用了xgboost模型，加入了均值和60/50/40/30/20/10分钟的飞机起飞数特征

--- common/models.py
+++ common/models.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+import matplotlib.pyplot as plt
 
 
 class RULES(object):
@@ -31,6 +32,7 @@ class RULES(object):
         # pre_value.to_csv("submissions/airport_gz_passenger_predict_mean.csv", index=True)
         print("Mean_solution_one END!")
         return pre_value
+
     def mean_solution_two(self):
         print("Mean_solution_two Run!")
         train_data = self.WIFI_Records
@@ -40,13 +42,14 @@ class RULES(object):
         train_data["passengerCount"] = (train_data["passengerCount"]/10).round(2)
         train_data["time"] = train_data["slice10min"].apply(lambda x: x[-4:])
         train_data = train_data.groupby(by=["time", "WIFIAPTag"])["passengerCount"].mean().round(2).reset_index()
-        pre_oldtime = [every_time[-4:] for every_time in self.pre_time]
+        # pre_oldtime = [every_time[-4:] for every_time in self.pre_time]
         train_data["slice10min"] = train_data["time"].apply(lambda x: "2016-09-14-"+x)
+
         del train_data["time"]
         train_data["slice10min"] == "2016-09-14-15-0"
         pre_value = train_data.loc[train_data["slice10min"].isin(self.pre_time),
                                    ["passengerCount", "WIFIAPTag", "slice10min"]]
-        pre_value.to_csv("submissions/train_data.csv", index=False)
+        # pre_value.to_csv("submissions/train_data.csv", index=False)
         '''
         print (pre_oldtime)
         # for group in train_data.groupby(by=["WIFIAPTag"]):
@@ -64,6 +67,51 @@ class RULES(object):
         # train_data.to_csv("clean_data/train_data.csv", index=False)
         print("Mean_solution_two END!")
         return pre_value
+
+    def median_solution(self):
+        print("Median_solution Run!")
+        train_data = self.WIFI_Records
+        train_data["slice10min"] = train_data["timeStamp"].apply(lambda x: x[0:15])
+        # train_data = train_data.sort_values(by=["WIFIAPTag", "slice10min"])
+        train_data = train_data.groupby(by=["WIFIAPTag", "slice10min"])["passengerCount"].sum().reset_index()
+        train_data["passengerCount"] = (train_data["passengerCount"] / 10).round(2)
+        train_data["time"] = train_data["slice10min"].apply(lambda x: x[-4:])
+        train_data = train_data.groupby(by=["time", "WIFIAPTag"])["passengerCount"].median().reset_index()
+        pre_oldtime = [every_time[-4:] for every_time in self.pre_time]
+        train_data["slice10min"] = train_data["time"].apply(lambda x: "2016-09-14-" + x)
+        del train_data["time"]
+        pre_value = train_data.loc[train_data["slice10min"].isin(self.pre_time),
+                                   ["passengerCount", "WIFIAPTag", "slice10min"]]
+        print("Median_solution END!")
+        return pre_value
+
+class XGBOOST(object):
+    def __init__(self, X_train, y_train, X_val, y_val):
+        dtrain = xgb.DMatrix(X_train, y_train)
+        dval = xgb.DMatrix(X_val, y_val)
+        num_round = 60
+        params = {"bst:max_depth": 7,
+                  "bst:eta": 0.01,
+                  "subsample": 0.8,
+                  "colsample_bytree": 1,
+                  "silent": 1,
+                  "objective": "reg:linear",
+                  "nthread": 6,
+                  "seed": 42}
+        evallist = [(dtrain, "train"), (dval, "val")]
+        self.bst = xgb.train(params, dtrain, num_round, evallist, feval=self.mae_xg,
+                             verbose_eval=10, early_stopping_rounds=10)
+        xgb.plot_importance(self.bst)
+        plt.show()
+        print("Mae on validation data: ", self.evaluate(X_val, y_val))
+
+    def evaluate(self, X_val, y_val):
+        y_val_pre = self.guess(X_val)
+        return np.mean(np.abs(np.expm1(y_val_pre) - np.expm1(y_val)))
+
+    def guess(self, X_test):
+        dtest = xgb.DMatrix(X_test)
+        return self.bst.predict(dtest)
 
 
 
--- rule_solution.py
+++ rule_solution.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import time
 from common.models import *
+from pandas.tseries.offsets import Hour, Minute
+from sklearn.cross_validation import train_test_split
 
 def plot_wifi_records(WIFI_Records):
     print("plot_wifi_records begain")
@@ -35,6 +37,19 @@ def rule_solution(filename, start_time, end_time):
                                )
     '''
     WIFI_Records = pd.read_csv(filename)
+    '''
+    test_time = ["2016-09-14-12", "2016-09-14-13", "2016-09-14-14"]
+    WIFI_Records_test = WIFI_Records.loc[~WIFI_Records["timeStamp"].apply(lambda x: x[0:13]).isin(test_time),
+                                   ["WIFIAPTag", "passengerCount", "timeStamp"]]
+    WIFI_Records_test_result = WIFI_Records.loc[WIFI_Records["timeStamp"].apply(lambda x: x[0:13]).isin(test_time),
+                                         ["WIFIAPTag", "passengerCount", "timeStamp"]]
+    WIFI_Records_test_result["slice10min"] = WIFI_Records_test_result["timeStamp"].apply(lambda x: x[0:15])
+    WIFI_Records_test_result = WIFI_Records_test_result.groupby(by=["WIFIAPTag", "slice10min"])["passengerCount"].sum().reset_index()
+    WIFI_Records_test_result["passengerCount"] = (WIFI_Records_test_result["passengerCount"] / 10).round(2)
+    WIFI_Records_test_result = pd.DataFrame(WIFI_Records_test_result, columns=['passengerCount', 'WIFIAPTag', 'slice10min'])
+    WIFI_Records_test.to_csv("clean_data/test12-15.csv", index=False)
+    WIFI_Records_test_result.to_csv("clean_data/test12-15_result.csv", index=False)
+    '''
     # WIFI_Records["timeStamp"] = WIFI_Records["timeStamp"].apply(lambda x: pd.Timestamp(x))
     pre_time = pd.date_range(start=start_time, end=end_time, freq="10min")
     pre_WIFIAPTag = WIFI_Records["WIFIAPTag"].drop_duplicates()
@@ -42,36 +57,221 @@ def rule_solution(filename, start_time, end_time):
     rule = RULES(WIFI_Records, pre_time, pre_WIFIAPTag)
 
     # 方法一：取均值一
-    # rule_res_mean = rule.mean_solution_one()
+    # rule_res = rule.mean_solution_one()
     # 方法二：取均值二
-    rule_res_mean = rule.mean_solution_two()
+    # rule_res = rule.mean_solution_two()
+    # 方法三：取中值
+    rule_res = rule.median_solution()
 
-    return rule_res_mean
+    return rule_res
+
+def extract_features(train_df, test_df):
+    # Combine train and test set
+    train_df['is_train'] = 1
+    test_df['is_train'] = 0
+    df = pd.concat([train_df, test_df], ignore_index=True)
+    # df.to_csv("clean_data/df.csv", index=False)
+    features_x = ["WIFIAPTag"]
+    features_y = "log_passengerCount"
+
+    # log_y
+    df["log_passengerCount"] = np.log1p(df["passengerCount"])
+
+    # -----------------------------
+    #  Add the time features
+    # -----------------------------
+    features_x.append('slice10min')
+
+    # ------------------------------
+    #  Add the bgate number features
+    # ------------------------------
+    bgate_df_org = pd.read_csv("../input/airport_gz_gates.csv")
+    bgate_df = bgate_df_org.groupby(by=["BGATE_AREA"])["BGATE_ID"].count().reset_index()
+    # print bgate_df
+    bgate_df.rename(columns={"BGATE_ID":"BGATE_num"}, inplace = True)
+    df["BGATE_AREA"] = df["WIFIAPTag"].apply(lambda x: x[0:2])
+    df = df.merge(bgate_df, how='left', on=["BGATE_AREA"]).fillna(0)
+    features_x.append("BGATE_num")
+
+    # ------------------------------
+    #  Add the mean features
+    # ------------------------------
+    mean_df = pd.read_csv("clean_data/train_data.csv")
+    mean_df.rename(columns={"passengerCount":"mean"}, inplace=True)
+    del mean_df["slice10min"]
+    df["time"] = df["slice10min"].apply(lambda x: x[-4:])
+    df = df.merge(mean_df, how="left", on=["WIFIAPTag", "time"])
+    del df["time"]
+    features_x.append("mean")
+
+    # -------------------------------
+    #  Add the flights number features
+    # -------------------------------
+    flights_df = pd.read_csv("../input/airport_gz_flights_chusai_1stround.csv",
+                             parse_dates=['scheduled_flt_time', 'actual_flt_time'],
+                             date_parser=(lambda dt: pd.to_datetime(dt, format='%Y/%m/%d %H:%M:%S') + 8*Hour() ))
+    # flights_df["scheduled_flt_time"] = flights_df["scheduled_flt_time"].apply(lambda x: pd.Timestamp(x) + 8 * Hour())
+    # flights_df["actual_flt_time"] = flights_df["actual_flt_time"].apply(lambda x: pd.Timestamp(x) + 8 * Hour())
+    # print flights_df["scheduled_flt_time"]
+    # 分情况讨论，如果有临时改变了登机口的，就按照最后一个登机口算
+    '''
+    flights_df["BGATE_ID"] = flights_df["BGATE_ID"].fillna(0)
+    for i in range(len(flights_df["BGATE_ID"])):
+        # print flights_df["BGATE_ID"][i]
+        if flights_df["BGATE_ID"][i]!=0:
+            if "," in flights_df["BGATE_ID"][i]:
+                flights_df["BGATE_ID"][i] = flights_df["BGATE_ID"][i].split(",")[1]
+
+    '''
+
+    flights_df = flights_df.merge(bgate_df_org, how='left', on=["BGATE_ID"]).fillna(0)
+    # flights_df[]
+    # flights_df["scheduled_flt_time-30"] = flights_df["scheduled_flt_time"]- 0.5*Hour()
+    # flights_df["actual_flt_time-20"] = flights_df["actual_flt_time"]-Hour()
+    flights_df["beforeScheduled_60"] = flights_df["scheduled_flt_time"] - Hour()
+    flights_df["beforeScheduled_50"] = flights_df["scheduled_flt_time"] - 50*Minute()
+    flights_df["beforeScheduled_40"] = flights_df["scheduled_flt_time"] - 40*Minute()
+    flights_df["beforeScheduled_30"] = flights_df["scheduled_flt_time"] - 30*Minute()
+    flights_df["beforeScheduled_20"] = flights_df["scheduled_flt_time"] - 20*Minute()
+    flights_df["beforeScheduled_10"] = flights_df["scheduled_flt_time"] - 10*Minute()
+    flights_df.to_csv("clean_data/flights_df.csv", index=False)
+
+    flights_df_60 = flights_df.groupby(by=["beforeScheduled_60", "BGATE_AREA"])["flight_ID"].count().reset_index()
+    flights_df_60.rename(columns={'flight_ID':"flightNum_60"}, inplace = True)
+    flights_df_60["slice10min"] = flights_df_60["beforeScheduled_60"].apply(
+        lambda x: str(x)[0:10]+"-"+str(x)[11:13]+"-"+str(x)[14:15])
+    del flights_df_60["beforeScheduled_60"]
+    df = df.merge(flights_df_60, how="left", on=['BGATE_AREA', 'slice10min']).fillna(0)
+
+    flights_df_50 = flights_df.groupby(by=["beforeScheduled_50", "BGATE_AREA"])["flight_ID"].count().reset_index()
+    flights_df_50.rename(columns={'flight_ID': "flightNum_50"}, inplace=True)
+    flights_df_50["slice10min"] = flights_df_50["beforeScheduled_50"].apply(
+        lambda x: str(x)[0:10] + "-" + str(x)[11:13] + "-" + str(x)[14:15])
+    del flights_df_50["beforeScheduled_50"]
+    df = df.merge(flights_df_50, how="left", on=['BGATE_AREA', 'slice10min']).fillna(0)
+
+    flights_df_40 = flights_df.groupby(by=["beforeScheduled_40", "BGATE_AREA"])["flight_ID"].count().reset_index()
+    flights_df_40.rename(columns={'flight_ID': "flightNum_40"}, inplace=True)
+    flights_df_40["slice10min"] = flights_df_40["beforeScheduled_40"].apply(
+        lambda x: str(x)[0:10] + "-" + str(x)[11:13] + "-" + str(x)[14:15])
+    del flights_df_40["beforeScheduled_40"]
+    df = df.merge(flights_df_40, how="left", on=['BGATE_AREA', 'slice10min']).fillna(0)
+
+    flights_df_30 = flights_df.groupby(by=["beforeScheduled_30", "BGATE_AREA"])["flight_ID"].count().reset_index()
+    flights_df_30.rename(columns={'flight_ID': "flightNum_30"}, inplace=True)
+    flights_df_30["slice10min"] = flights_df_30["beforeScheduled_30"].apply(
+        lambda x: str(x)[0:10] + "-" + str(x)[11:13] + "-" + str(x)[14:15])
+    del flights_df_30["beforeScheduled_30"]
+    df = df.merge(flights_df_30, how="left", on=['BGATE_AREA', 'slice10min']).fillna(0)
+
+    flights_df_20 = flights_df.groupby(by=["beforeScheduled_20", "BGATE_AREA"])["flight_ID"].count().reset_index()
+    flights_df_20.rename(columns={'flight_ID': "flightNum_20"}, inplace=True)
+    flights_df_20["slice10min"] = flights_df_20["beforeScheduled_20"].apply(
+        lambda x: str(x)[0:10] + "-" + str(x)[11:13] + "-" + str(x)[14:15])
+    del flights_df_20["beforeScheduled_20"]
+    df = df.merge(flights_df_20, how="left", on=['BGATE_AREA', 'slice10min']).fillna(0)
+
+    flights_df_10 = flights_df.groupby(by=["beforeScheduled_10", "BGATE_AREA"])["flight_ID"].count().reset_index()
+    flights_df_10.rename(columns={'flight_ID': "flightNum_10"}, inplace=True)
+    flights_df_10["slice10min"] = flights_df_10["beforeScheduled_10"].apply(
+        lambda x: str(x)[0:10] + "-" + str(x)[11:13] + "-" + str(x)[14:15])
+    del flights_df_10["beforeScheduled_10"]
+    df = df.merge(flights_df_10, how="left", on=['BGATE_AREA', 'slice10min']).fillna(0)
+    del df["BGATE_AREA"]
+
+    # flights_df_60.to_csv("clean_data/flights_df_60.csv", index=False)
+    df.to_csv("clean_data/df.csv", index=False)
+    features_x.append("flightNum_60")
+    features_x.append("flightNum_50")
+    features_x.append("flightNum_40")
+    features_x.append("flightNum_30")
+    features_x.append("flightNum_20")
+    features_x.append("flightNum_10")
+
+
+
+
+
+    return df, features_x, features_y
+
+def model_solution(model_name="xgb"):
+    print("model_solution run!")
+    '''
+    train_df = pd.read_csv("clean_data/test12-15.csv")
+    train_df = train_df.loc[~train_df["timeStamp"].apply(lambda x: x[0:10]).isin(["2016-09-10"]),
+                            ["WIFIAPTag", "passengerCount", "timeStamp"]]# 把9月10号的数据去除掉
+    train_df["slice10min"] = train_df["timeStamp"].apply(lambda x: x[0:15])
+    train_df = train_df.groupby(by=["WIFIAPTag", "slice10min"])["passengerCount"].sum().reset_index()
+    train_df["passengerCount"] = (train_df["passengerCount"]/10).round(2)
+    train_df.to_csv("clean_data/train_df.csv", index=False)
+    '''
+    train_df = pd.read_csv("clean_data/train_df.csv")
+    # Construct test df
+    test_ids = train_df["WIFIAPTag"].drop_duplicates()
+    test_ids = pd.DataFrame(test_ids, columns=['WIFIAPTag']).reset_index()
+    del test_ids['index']
+    test_ids['passengerCount'] = 0
+    # print test_ids
+    pre_time = pd.date_range(start="20160914120000", end="20160914145959", freq="10min")
+    pre_time = [w.strftime("%Y-%m-%d-%H-%M") for w in pre_time]
+    pre_time = [w[:-1] for w in pre_time]
+    pre_time = pd.DataFrame(pre_time, columns=['slice10min'])
+    pre_time['passengerCount'] = 0
+    # print pre_time
+    test_df = pd.merge(test_ids, pre_time, how='left', on='passengerCount')
+    # print test_df
+    test_df.to_csv("clean_data/test_df.csv", index=False)
+
+    # 提取特征
+    df, features_x, features_y = extract_features(train_df, test_df)  # 提取特征
+    print features_x
+    X_train, X_val, y_train, y_val = train_test_split(df.loc[df["is_train"] == 1][features_x],
+                                                      df.loc[df["is_train"] == 1][features_y],
+                                                      test_size=0.01, random_state=42)
+    X_test = df.loc[df["is_train"] == 0, features_x]  # 建立测试集
+    if model_name == "xgb":  # 通过参数构造模型
+        model = XGBOOST(X_train, y_train, X_val, y_val)
+
+
+    y_pre = model.guess(X_test)
+
+    result_df = test_df[["WIFIAPTag", "slice10min"]].reset_index()
+    del result_df["index"]
+    if model_name == "xgb":
+        result_df["passengerCount"] = (np.expm1(y_pre) * 1).astype(np.int)
+    return result_df
 
 def test(result):
-    test_result = pd.read_csv("clean_data/airport_gz_passenger_predict_standard.csv")
-    Criteria = sum((result['passengerCount']-test_result['passengerCount'])*(result['passengerCount']-test_result['passengerCount']))
-    print("Criteria = %f" % Criteria)
+    test_result = pd.read_csv("clean_data/test12-15_result.csv")
+    test_result.rename(columns={"passengerCount":"passengerCount_result"}, inplace=True)
+    test_result = test_result.merge(result, how='left', on=["WIFIAPTag", "slice10min"]).fillna(0)
+    test_result.to_csv("clean_data/test_result.csv", index=False)
+    # Criteria = sum((result['passengerCount']-test_result['passengerCount'])*(result['passengerCount']-test_result['passengerCount']))
+    score = sum((test_result["passengerCount"] - test_result["passengerCount_result"])*(test_result["passengerCount"] - test_result["passengerCount_result"]))
+    print("score = %f" % score)
 
 if __name__ == "__main__":
     time0 = time.time()
-    '''
+
     # 测试集
-    filename = "clean_data/test_train.csv"
+    filename = "clean_data/test12-15.csv"
     start_time = "20160914120000"
     end_time = "20160914145959"
-    '''
-
 
     # 真正训练集
+    '''
     filename = "../input/WIFI_AP_Passenger_Records_chusai_1stround.csv"
     start_time = "20160914150000"
     end_time = "20160914175959"
+    '''
 
 
-    rule_result = rule_solution(filename, start_time, end_time)
-    rule_result.to_csv("submissions/airport_gz_passenger_predict_mean_two.csv", index=False)
+    # rule_result = rule_solution(filename, start_time, end_time)
+    # rule_result.to_csv("submissions/airport_gz_passenger_predict_rule_median.csv", index=False)
 
-    # test(rule_result)
+    model1_result = model_solution("xgb")
+    model1_result.to_csv("submissions/airport_gz_passenger_predict_model_xgb.csv", index=False)
+
+    test(model1_result)
     time1 = time.time()
     print ("It take %f s to process" % (time1 - time0))
